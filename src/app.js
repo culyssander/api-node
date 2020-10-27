2b11a185467ee9dff348867e1277bfb29f8044e9
Quitumba Culyssander Ferreira <culyssandercentro@gmail.com>
2020-10-27 05:15:05 +0100

    Criando o crud - create, read, update and delete (post, get, put, delete)

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 'use strict'
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
 const router = express.Router();
+
 
 const route = router.get('/', (req, res, next) => {
     res.status(200).send({
@@ -13,6 +18,27 @@ const route = router.get('/', (req, res, next) => {
     });
 });
 
+const create = router.post('/', (req, res, next) => {
+    res.status(201).send(req.body);
+});
+
+const update = router.put('/:id', (req, res, next) => {
+    const id = req.params.id;
+    res.status(200).send({
+        id: id,
+        item: req.body
+    });
+});
+
+const del = router.delete('/:id', (req, res, next) => {
+    const id = req.params.id;
+    console.log('delentado sobre o id: ' + id);
+    res.status(204).send();
+});
+
 app.use('/', route);
+app.use('/', create);
+app.use('/', update);
+app.use('/', del)
 
 module.exports = app
